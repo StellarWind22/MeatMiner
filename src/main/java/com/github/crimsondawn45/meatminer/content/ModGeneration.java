b3b63c4ea02c56c08671a4a287a517738def426d
CrimsonDawn45 <gannonburks45@gmail.com>
2022-12-04 01:29:51 -0600

    Turned ore generation into a util class

--- src/main/java/com/github/crimsondawn45/meatminer/content/ModGeneration.java
+++ src/main/java/com/github/crimsondawn45/meatminer/content/ModGeneration.java
@@ -1,54 +1,17 @@
 package com.github.crimsondawn45.meatminer.content;
 
-import java.util.Arrays;
-
-import com.github.crimsondawn45.meatminer.MeatMinerInit;
 import com.github.crimsondawn45.meatminer.util.MContent;
+import com.github.crimsondawn45.meatminer.util.MOreGenerator;
 
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.OreConfiguredFeatures;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.PlacedFeature;
-import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
-import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
-import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
 public class ModGeneration extends MContent {
 
-    private static ConfiguredFeature<? extends Feature<FeatureConfig>, ? extends FeatureConfig> OVERWORLD_WOOL_ORE_CONFIGURED_FEATURE = new ConfiguredFeature(
-        Feature.ORE,
-        new OreFeatureConfig(
-            OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
-            ModBlocks.meat_ore.getBlock().getDefaultState(),
-            4)); // vein size
-
-    public static PlacedFeature OVERWORLD_MEAT_ORE_PLACED_FEATURE;
+    MOreGenerator overworldMeatOre;
      
     @Override
     public void registerContent(){
-
-        OVERWORLD_MEAT_ORE_PLACED_FEATURE = new PlacedFeature(
-            RegistryEntry.of(OVERWORLD_WOOL_ORE_CONFIGURED_FEATURE),
-            Arrays.asList(
-                CountPlacementModifier.of(20), // number of veins per chunk
-                SquarePlacementModifier.of(), // spreading horizontally
-                HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64))
-            )); // height
-
-        //Register features
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MeatMinerInit.MOD_ID, "overworld_meat_ore"), OVERWORLD_WOOL_ORE_CONFIGURED_FEATURE);
-        Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(MeatMinerInit.MOD_ID, "overworld_meat_ore"), OVERWORLD_MEAT_ORE_PLACED_FEATURE);
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(MeatMinerInit.MOD_ID, "overworld_meat_ore")));
+        overworldMeatOre = new MOreGenerator(ModBlocks.meat_ore, BiomeSelectors.foundInOverworld(), 10, 8, YOffset.BOTTOM, YOffset.TOP);
     }
 }
---
+++ src/main/java/com/github/crimsondawn45/meatminer/util/MOreGenerator.java
@@ -0,0 +1,54 @@
+package com.github.crimsondawn45.meatminer.util;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import com.github.crimsondawn45.meatminer.MeatMinerInit;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.FeatureConfig;
+import net.minecraft.world.gen.feature.OreConfiguredFeatures;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.PlacedFeature;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
+
+public class MOreGenerator {
+
+    private static ConfiguredFeature<? extends Feature<FeatureConfig>, ? extends FeatureConfig> ORE_CONFIGURED_FEATURE;
+    public static PlacedFeature ORE_PLACED_FEATURE;
+
+    public MOreGenerator(MBlock ore, Predicate<BiomeSelectionContext> biomeSelector, int veinsPerChunk, int veinSize, YOffset min, YOffset max) {
+
+        ORE_CONFIGURED_FEATURE = new ConfiguredFeature(
+            Feature.ORE,
+            new OreFeatureConfig(
+                OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
+                ore.getBlock().getDefaultState(),  // ore
+                veinSize)); // vein size
+
+        ORE_PLACED_FEATURE = new PlacedFeature(
+            RegistryEntry.of(ORE_CONFIGURED_FEATURE),
+            Arrays.asList(
+                CountPlacementModifier.of(veinsPerChunk), // number of veins per chunk
+                SquarePlacementModifier.of(), // spreading horizontally
+                HeightRangePlacementModifier.uniform(min, max)
+            )); // height
+
+        //Register features
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(MeatMinerInit.MOD_ID, "ore"), ORE_CONFIGURED_FEATURE);
+        Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(MeatMinerInit.MOD_ID, "ore"), ORE_PLACED_FEATURE);
+        BiomeModifications.addFeature(biomeSelector, GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(MeatMinerInit.MOD_ID, "ore")));
+    }
+}
