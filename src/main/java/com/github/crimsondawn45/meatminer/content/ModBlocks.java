e13dc823c0450dd7096476d81aa273baa28f67ae
CrimsonDawn45 <gannonburks45@gmail.com>
2022-12-03 21:15:32 -0600

    i'll work on this later

--- src/main/java/com/github/crimsondawn45/meatminer/content/ModBlocks.java
+++ src/main/java/com/github/crimsondawn45/meatminer/content/ModBlocks.java
@@ -1,11 +1,17 @@
 package com.github.crimsondawn45.meatminer.content;
 
+import com.github.crimsondawn45.meatminer.util.MBlock;
 import com.github.crimsondawn45.meatminer.util.MContent;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 
 public class ModBlocks extends MContent {
 
+    public static MBlock meat_ore;
+
     @Override
     public void registerContent(){
-        
+        meat_ore = new MBlock("meat_ore", new BlockItem(new Block(null), null));
     }
 }
