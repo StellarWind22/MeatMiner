2446caf0d476b9c284d63d11de7f7276dcf1d588
StellarWind22 <gannonburks45@gmail.com>
2025-09-20 20:48:38 -0500

    Added broken stairs...

--- common/src/main/java/com/stellarwind22/meatminer/config/MeatMinerConfig.java
+++ common/src/main/java/com/stellarwind22/meatminer/config/MeatMinerConfig.java
@@ -5,5 +5,8 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class MeatMinerConfig extends MidnightConfig {
 
     @Entry
+    public static boolean modify_trades = true;
+
+    @Entry
     public static int emerald_conversion_rate = 1;
 }
--- common/src/main/java/com/stellarwind22/meatminer/content/MeatMinerItems.java
+++ common/src/main/java/com/stellarwind22/meatminer/content/MeatMinerItems.java
@@ -24,6 +24,8 @@ public class MeatMinerItems {
     public static RegistrySupplier<Item> MEAT_NUGGET;
     public static RegistrySupplier<Item> MEAT;
     public static RegistrySupplier<BlockItem> MEAT_BLOCK;
+    public static RegistrySupplier<BlockItem> MEAT_STAIRS;
+    public static RegistrySupplier<BlockItem> MEAT_SLAB;
     //Ore
     public static RegistrySupplier<BlockItem> MEAT_ORE;
     public static RegistrySupplier<BlockItem> DEEPSLATE_MEAT_ORE;
@@ -35,6 +37,8 @@ public class MeatMinerItems {
     public static RegistrySupplier<Item> COOKED_MEAT_NUGGET;
     public static RegistrySupplier<Item> COOKED_MEAT;
     public static RegistrySupplier<BlockItem> COOKED_MEAT_BLOCK;
+    public static RegistrySupplier<BlockItem> COOKED_MEAT_STAIRS;
+    public static RegistrySupplier<BlockItem> COOKED_MEAT_SLAB;
     //Ore
     public static RegistrySupplier<BlockItem> COOKED_MEAT_ORE;
     public static RegistrySupplier<BlockItem> DEEPSLATE_COOKED_MEAT_ORE;
@@ -52,6 +56,8 @@ public class MeatMinerItems {
         MEAT_NUGGET = registerItem("meat_nugget", props -> new Item(props.food(MeatMinerFoods.MEAT_NUGGET, MeatMinerFoods.MEAT_NUGGET_CONSUMABLE).trimMaterial(MEAT_TRIM)));
         MEAT = registerItem("meat", props -> new Item(props.food(MeatMinerFoods.MEAT).trimMaterial(MEAT_TRIM)));
         MEAT_BLOCK = registerItem("meat_block", props -> new BlockItem(MeatMinerBlocks.MEAT_BLOCK.get(), props.food(MeatMinerFoods.MEAT_BLOCK, MeatMinerFoods.MEAT_BLOCK_CONSUMABLE)));
+        MEAT_STAIRS = registerItem("meat_stairs", props -> new BlockItem(MeatMinerBlocks.MEAT_STAIRS.get(), props.food(MeatMinerFoods.MEAT_BLOCK, MeatMinerFoods.MEAT_BLOCK_CONSUMABLE)));
+        MEAT_SLAB = registerItem("meat_slab", props -> new BlockItem(MeatMinerBlocks.MEAT_SLAB.get(), props.food(MeatMinerFoods.MEAT_BLOCK, MeatMinerFoods.MEAT_BLOCK_CONSUMABLE)));
         //Ore
         MEAT_ORE = registerItem("meat_ore", props -> new BlockItem(MeatMinerBlocks.MEAT_ORE.get(), props));
         DEEPSLATE_MEAT_ORE = registerItem("deepslate_meat_ore", props -> new BlockItem(MeatMinerBlocks.DEEPSLATE_MEAT_ORE.get(), props));
@@ -63,6 +69,8 @@ public class MeatMinerItems {
         COOKED_MEAT_NUGGET = registerItem("cooked_meat_nugget", props -> new Item(props.food(MeatMinerFoods.COOKED_MEAT_NUGGET, MeatMinerFoods.MEAT_NUGGET_CONSUMABLE).trimMaterial(COOKED_MEAT_TRIM)));
         COOKED_MEAT = registerItem("cooked_meat", props -> new Item(props.food(MeatMinerFoods.COOKED_MEAT).trimMaterial(COOKED_MEAT_TRIM)));
         COOKED_MEAT_BLOCK = registerItem("cooked_meat_block", props -> new BlockItem(MeatMinerBlocks.COOKED_MEAT_BLOCK.get(), props.food(MeatMinerFoods.COOKED_MEAT_BLOCK, MeatMinerFoods.MEAT_BLOCK_CONSUMABLE)));
+        COOKED_MEAT_STAIRS = registerItem("cooked_meat_stairs", props -> new BlockItem(MeatMinerBlocks.COOKED_MEAT_STAIRS.get(), props.food(MeatMinerFoods.COOKED_MEAT_BLOCK, MeatMinerFoods.MEAT_BLOCK_CONSUMABLE)));
+        COOKED_MEAT_SLAB = registerItem("cooked_meat_slab", props -> new BlockItem(MeatMinerBlocks.COOKED_MEAT_SLAB.get(), props.food(MeatMinerFoods.COOKED_MEAT_BLOCK, MeatMinerFoods.MEAT_BLOCK_CONSUMABLE)));
         //Ore
         COOKED_MEAT_ORE = registerItem("cooked_meat_ore", props -> new BlockItem(MeatMinerBlocks.COOKED_MEAT_ORE.get(), props));
         DEEPSLATE_COOKED_MEAT_ORE = registerItem("deepslate_cooked_meat_ore", props -> new BlockItem(MeatMinerBlocks.DEEPSLATE_COOKED_MEAT_ORE.get(), props));
