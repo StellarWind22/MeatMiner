cb323fcd269d60eb932a4797241f802e6c8f0d51
StellarWind22 <gannonburks45@gmail.com>
2025-09-22 14:35:49 -0500

    Updated mixin x2

--- common/src/main/java/com/stellarwind22/meatminer/mixin/MerchantOfferMixin.java
+++ common/src/main/java/com/stellarwind22/meatminer/mixin/MerchantOfferMixin.java
@@ -33,7 +33,8 @@ public abstract class MerchantOfferMixin {
                             count
                     )
             );
-        } else if(stack.is(Items.EMERALD_BLOCK)) {
+        }
+        if(stack.is(Items.EMERALD_BLOCK)) {
             cir.setReturnValue(
                     new ItemStack(
                             new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
@@ -55,7 +56,8 @@ public abstract class MerchantOfferMixin {
                             count * MeatMinerConfig.emerald_conversion_rate
                     )
             );
-        } else if(stack.is(Items.EMERALD_BLOCK)) {
+        }
+        if(stack.is(Items.EMERALD_BLOCK)) {
             cir.setReturnValue(
                     new ItemStack(
                             new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
@@ -77,7 +79,8 @@ public abstract class MerchantOfferMixin {
                             count * MeatMinerConfig.emerald_conversion_rate
                     )
             );
-        } else if(stack.is(Items.EMERALD_BLOCK)) {
+        }
+        if(stack.is(Items.EMERALD_BLOCK)) {
             cir.setReturnValue(
                     new ItemStack(
                             new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
@@ -99,7 +102,8 @@ public abstract class MerchantOfferMixin {
                             count * MeatMinerConfig.emerald_conversion_rate
                     )
             );
-        } else if(item.equals(Items.EMERALD_BLOCK)) {
+        }
+        if(item.equals(Items.EMERALD_BLOCK)) {
             cir.setReturnValue(
                     new ItemCost(
                             new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
@@ -123,7 +127,8 @@ public abstract class MerchantOfferMixin {
                                 count * MeatMinerConfig.emerald_conversion_rate
                         ))
                 );
-            } else if(item.equals(Items.EMERALD_BLOCK)) {
+            }
+            if(item.equals(Items.EMERALD_BLOCK)) {
                 cir.setReturnValue(
                         Optional.of(new ItemCost(
                                 new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
@@ -146,7 +151,8 @@ public abstract class MerchantOfferMixin {
                             count * MeatMinerConfig.emerald_conversion_rate
                     )
             );
-        } else if(stack.is(Items.EMERALD_BLOCK)) {
+        }
+        if(stack.is(Items.EMERALD_BLOCK)) {
             cir.setReturnValue(
                     new ItemStack(
                             new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
@@ -168,7 +174,8 @@ public abstract class MerchantOfferMixin {
                             count * MeatMinerConfig.emerald_conversion_rate
                     )
             );
-        } else if(stack.is(Items.EMERALD_BLOCK)) {
+        }
+        if(stack.is(Items.EMERALD_BLOCK)) {
             cir.setReturnValue(
                     new ItemStack(
                             new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
@@ -181,15 +188,12 @@ public abstract class MerchantOfferMixin {
     @Final @Shadow
     private ItemCost baseCostA;
 
-    @Shadow
-    public abstract ItemCost getItemCostA();
-
     @Inject(at = @At("RETURN"), method = "satisfiedBy", cancellable = true)
     public void satisfiedBy(ItemStack itemStack, ItemStack itemStack2, CallbackInfoReturnable<Boolean> cir) {
         if(itemStack.is(Items.EMERALD) || itemStack.is(Items.EMERALD_BLOCK) || itemStack2.is(Items.EMERALD) || itemStack2.is(Items.EMERALD_BLOCK)) {
             MerchantOffer self = (MerchantOffer) (Object) this;
 
-            if (self.getItemCostA().test(itemStack) && itemStack.getCount() >= meatminer$getModifiedCostCount(self, this.baseCostA)) {
+            if (self.getItemCostA().test(itemStack) && itemStack.getCount() >= meatminer$getModifiedCostCount(self, this.meatminer$getActualBaseCostA())) {
                 if (self.getItemCostB().isEmpty()) {
                     cir.setReturnValue(itemStack2.isEmpty());
                 } else {
@@ -207,5 +211,25 @@ public abstract class MerchantOfferMixin {
         int i = itemCost.count();
         int j = Math.max(0, Mth.floor((float)(i * self.getDemand()) * self.getPriceMultiplier()));
         return Mth.clamp(i + j + self.getSpecialPriceDiff(), 1, itemCost.itemStack().getMaxStackSize());
+    }
+
+    @Unique
+    private ItemCost meatminer$getActualBaseCostA() {
+        var cost = this.baseCostA;
+        var count = cost.count();
+        var item = cost.item().value();
+        if(item.equals(Items.EMERALD)) {
+            return new ItemCost(
+                    new DeferredItem<>(MeatMinerItems.MEAT),
+                    count * MeatMinerConfig.emerald_conversion_rate
+            );
+        }
+        if(item.equals(Items.EMERALD_BLOCK)) {
+            return new ItemCost(
+                    new DeferredItem<>(MeatMinerItems.MEAT_BLOCK),
+                    (count * 9) * MeatMinerConfig.emerald_conversion_rate / 4
+            );
+        }
+        return cost;
     }
 }
