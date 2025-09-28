a028c233dc480ad9077655eea517f92250d5e27c
StellarWind22 <gannonburks45@gmail.com>
2025-09-28 09:37:07 -0500

    Added item model for trimmed armors(need to add overlay to leather ones)

--- fabric/src/main/java/com/stellarwind22/meatminer/fabric/init/MeatMinerFabric.java
+++ fabric/src/main/java/com/stellarwind22/meatminer/fabric/init/MeatMinerFabric.java
@@ -1,11 +1,17 @@
 package com.stellarwind22.meatminer.fabric.init;
 
+import com.stellarwind22.meatminer.config.MeatMinerConfig;
 import com.stellarwind22.meatminer.init.MeatMiner;
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 
 public final class MeatMinerFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         MeatMiner.init();
+
+        MidnightConfig.init(MeatMiner.MOD_ID, MeatMinerConfig.class);
+
+        MeatMiner.LOGGER.info("MeatMiner initialized!");
     }
 }
--- neoforge/src/main/java/com/stellarwind22/meatminer/neoforge/init/MeatMinerNeoForge.java
+++ neoforge/src/main/java/com/stellarwind22/meatminer/neoforge/init/MeatMinerNeoForge.java
@@ -1,11 +1,34 @@
 package com.stellarwind22.meatminer.neoforge.init;
 
+import com.stellarwind22.meatminer.config.MeatMinerConfig;
 import com.stellarwind22.meatminer.init.MeatMiner;
+import com.stellarwind22.meatminer.init.MeatMinerClient;
+import eu.midnightdust.lib.config.MidnightConfig;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(MeatMiner.MOD_ID)
 public final class MeatMinerNeoForge {
     public MeatMinerNeoForge() {
         MeatMiner.init();
+
+        if(FMLLoader.getDist() == Dist.CLIENT) {
+            MeatMinerClient.init();
+        }
+
+        MeatMiner.LOGGER.info("MeatMiner initialized!");
+    }
+
+    @EventBusSubscriber
+    public static class Events {
+
+        @SubscribeEvent
+        public static void setup(FMLCommonSetupEvent event) {
+            MidnightConfig.init(MeatMiner.MOD_ID, MeatMinerConfig.class);
+        }
     }
 }
